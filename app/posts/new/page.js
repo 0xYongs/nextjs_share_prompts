767a6b5cfcfb311c81008fa1df025edfb5c37984
0xYongs <tradingsensei21@gmail.com>
2023-06-21 13:30:09 +0200

    Exploring the routing ways of NextJS by creating routes and also nesting them accordingly

---
+++ app/posts/new/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// /localhost:3000/posts/new
+
+const new_post = () => {
+    return (
+        <div> Render all the new posts in this page </div>
+    )
+}
+
+export default new_post
---
+++ app/posts/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+/*  /localhost:3000/posts
+    AKA /pages/posts
+*/
+
+const page = () => {
+    return (
+        <div> Render all the Posts</div>
+    )
+}
+
+export default page
