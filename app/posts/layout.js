6ffcbd6ff08f01518144491859b4bfc21654199d
0xYongs <tradingsensei21@gmail.com>
2023-06-21 13:59:39 +0200

    Create a new layout under posts directory to improve NavBar and footer

---
+++ app/posts/layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+/*
+    Modify layout of the posts pages by setting this
+    layout file as subfolder to posts.
+
+    This presents an oportunity to showcase more
+    branding and mantain a neat intuitive display.
+
+    The NavBar can now show post_id, new_post and posts
+    Footer also can be modified, below <div> as example
+*/
+
+const layout = () => {
+    return (
+        <div>Navigate to Top (button) as example</div>
+    )
+}
+
+export default layout
